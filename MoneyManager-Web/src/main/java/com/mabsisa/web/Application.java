8ec1ea18ec54a619bf250efc8f48da1db88f5015
abhinabkanrar <abhinabkanrar@gmail.com>
2017-05-02 16:15:25 +0530

    adding default data adding functionality

--- MoneyManager-Web/src/main/java/com/mabsisa/web/Application.java
+++ MoneyManager-Web/src/main/java/com/mabsisa/web/Application.java
@@ -5,7 +5,10 @@ package com.mabsisa.web;
 
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.boot.autoconfigure.jms.JmsAutoConfiguration;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Import;
 
@@ -16,16 +19,19 @@ import com.mabsisa.service.ApplicationServiceContext;
  *
  */
 @SpringBootApplication
-@ComponentScan(basePackages="com.mabsisa.web")
+@ComponentScan(basePackages = "com.mabsisa.web")
 @Import(ApplicationServiceContext.class)
+@EnableAutoConfiguration(exclude = { DataSourceAutoConfiguration.class, JmsAutoConfiguration.class })
 public class Application implements CommandLineRunner {
 
-	/* (non-Javadoc)
+	/*
+	 * (non-Javadoc)
+	 * 
 	 * @see org.springframework.boot.CommandLineRunner#run(java.lang.String[])
 	 */
 	@Override
 	public void run(String... args) throws Exception {
-		
+
 	}
 
 	/**
